7211df196c0762e87b3fcf679b02fa1723776ac7
muaaziqbalvip <muaaziqbal.vip@gmail.com>
2026-04-15 06:41:54 +0500

    Add files via upload

---
+++ src/components/chatUI.js
@@ -0,0 +1,2 @@
+// MI AI - Chat UI Component stub (logic in app.js)
+// By Muaaz Iqbal | Muslim Islam Org
---
+++ src/components/codeBlock.js
@@ -0,0 +1,2 @@
+// MI AI - Code Block Component stub (logic in app.js)
+// By Muaaz Iqbal | Muslim Islam Org
